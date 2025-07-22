9f77a72e6f6cac892d81651e7a5a0d0db1174e9f
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-07-22 11:53:21 +0200

    fix: correct HTML structure in card rendering ! add an animation when hovering on cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,17 +8,18 @@ for (let i = 0; i < 6; i++) {
   axios.get('https://lanciweb.github.io/demo/api/pictures').then((res) => {
     const card = res.data
     cardContainer.innerHTML += `<div class="col-12 col-md-6 col-lg-4">
-      <div div class="position-relative" >
+      <div class="position-relative" >
         <div class="card p-3 rounded-0">
               <img src="${card[i].url}" class="card-img-top rounded-0" alt="...">
               <div class="card-body">  
               <p class="card-text">${card[i].date}</p>
               <p class="card-text"><strong class="fs-4">${card[i].title}</strong></p>
               </div>
-            </div>
-            <img src="./assets/img/pin.svg" class="position-absolute top-0 start-50 translate-middle " alt="">
-          </div>
-      </div>`
+        </div>
+        <img src="./assets/img/pin.svg" class="position-absolute top-0 start-50 translate-middle" 
+        alt="pin" id="pin">
+      </div>
+    </div>`
 
     const cards = document.querySelectorAll('.card');
 
